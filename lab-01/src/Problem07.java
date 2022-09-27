40dafce739d584004602bf31eb0b29720cd8c071
Nargis <sarwari_n@auca.kg>
2022-09-27 23:37:15 +0600

    problem07, Lab-01

---
+++ lab-01/src/Problem07.java
@@ -0,0 +1,13 @@
+public class Problem07 {
+    public static void main(String[] args) {
+        System.out.println("a    a^2    a^3");
+        System.out.print(1);System.out.print("    ");System.out.print((1*1));System.out.print("      ");
+        System.out.println((1*1*1));
+        System.out.print(2);System.out.print("    ");System.out.print((2*2));System.out.print("      ");
+        System.out.println(2*2*2);
+        System.out.print(3); System.out.print("    "); System.out.print((3*3));System.out.print("      ");
+        System.out.println((3*3*3));
+        System.out.print(4); System.out.print("    "); System.out.print((4*4)); System.out.print("     ");
+        System.out.print((4*4*4));
+    }
+}
