4b89eb61225e4cf374a7109a25acb48904af1208
Nargis <sarwari_n@auca.kg>
2022-09-27 18:28:24 +0600

    problem03, Lab-01

---
+++ lab-01/src/Problem03.java
@@ -0,0 +1,16 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Problem03 {
+    public static void main(String[] args) {
+        System.out.println("what is your name?");
+        Scanner input = new Scanner(System.in);
+        String username = input.nextLine();
+        System.out.println(" hello, " + username + "!");
+        System.out.println("Your name in upper-case:" + username.toUpperCase());
+        System.out.println("Your name in lower-case:"+ username.toLowerCase());
+        System.out.println("The total number of characters is" +username.length());
+        System.out.println(username.length());
+
+    }
+}
