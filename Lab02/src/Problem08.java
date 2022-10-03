1a8cd1ead1eac210275da6209d54248187c6c1b6
Nargis <sarwari_n@auca.kg>
2022-10-03 13:36:08 +0600

    Problem08, Lab-01

---
+++ Lab02/src/Problem08.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Problem08 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number in pounds: ");
+        double pounds = input.nextDouble();
+        double kilograms = 0.454 * pounds;
+        System.out.println("pounds " + pounds + " is " + kilograms + " in kilograms");
+
+    }
+}
