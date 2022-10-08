a39b46c644f73c921f17d036cceae1713c53b7fa
Nargis <sarwari_n@auca.kg>
2022-10-08 17:59:42 +0600

    Completed Problem10, Lab-01

--- lab-01/src/Problem10.java
+++ lab-01/src/Problem10.java
@@ -3,15 +3,15 @@ public class Problem10 {
         final int POPULATION = 312_032_486;
         final int  SEC_IN_YEAR = 365 * 24 * 60 * 60;
 
-        double birth = SEC_IN_YEAR / 7.;
-        double death = SEC_IN_YEAR / 13.;
-        double migration = SEC_IN_YEAR / 445.;
-        double yearlypopulationgrowth = birth + death + migration;
+        double birth = SEC_IN_YEAR / 7.0;
+        double death = SEC_IN_YEAR / 13.0;
+        double migration = SEC_IN_YEAR / 45.0;
+        double yearlypopulationgrowth = birth - death + migration;
         int popInt1Year = (int) Math.round(POPULATION +yearlypopulationgrowth);
-        int popInt2Year = (int) Math.round(POPULATION + yearlypopulationgrowth);
-        int popInt3Year = (int) Math.round(POPULATION + yearlypopulationgrowth);
-        int popInt4Year = (int) Math.round(POPULATION + yearlypopulationgrowth);
-        int popInt5Year = (int) Math.round(POPULATION + yearlypopulationgrowth);
+        int popInt2Year = (int) Math.round(POPULATION + yearlypopulationgrowth * 2);
+        int popInt3Year = (int) Math.round(POPULATION + yearlypopulationgrowth * 3);
+        int popInt4Year = (int) Math.round(POPULATION + yearlypopulationgrowth * 4);
+        int popInt5Year = (int) Math.round(POPULATION + yearlypopulationgrowth * 5);
 
         System.out.println(
                 popInt1Year + "\n" +
