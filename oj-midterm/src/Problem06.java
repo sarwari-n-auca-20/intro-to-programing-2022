0f3a824ae3a521d82c9ff4411f7e8c647f783715
Nargis <sarwari_n@auca.kg>
2022-10-11 23:31:00 +0600

    Accepted oj-midterm's Problem06

---
+++ oj-midterm/src/Problem06.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Problem06 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int width = input.nextInt();
+        int length = input.nextInt();
+        int sideLength = input.nextInt();
+        int div = width / sideLength;
+        int divLength = length / sideLength;
+        if (width >=1 && width <= 1000)
+            if (length >= 1 && length <= 1000)
+                if (sideLength >= 1 && sideLength <= 1000)
+        System.out.println(div * divLength);
+    }
+}
+
