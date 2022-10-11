0234d1942a6e4e9d2c47ecd64201c0acd557a030
Nargis <sarwari_n@auca.kg>
2022-10-11 23:07:38 +0600

    Accepted oj-midterm's Problem07

--- oj-midterm/src/Problem07.java
+++ oj-midterm/src/Problem07.java
@@ -5,6 +5,8 @@ public class Problem07 {
         int R1 = input.nextInt();
         int S = input.nextInt();
         int R2 = 2*S - R1;
+        if (R1 >=-1000 && R1 <= 1000)
+            if (S >=-1000 && S <= 1000)
         System.out.println(R2);
     }
 }
