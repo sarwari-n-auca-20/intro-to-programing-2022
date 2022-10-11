216ea0cb7c8459e0b3c3e1492e0822d2bf08abe5
Nargis <sarwari_n@auca.kg>
2022-10-11 22:38:32 +0600

    Accepted oj-midterm's Problem08

---
+++ oj-midterm/src/Problem08.java
@@ -0,0 +1,17 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Problem08 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt();
+        int T = input.nextInt();
+        int M = input.nextInt();
+        if (N >=1 && N <= 500);
+        if (T >=1 && T <= 500);
+        if (M >=1 && M <= 500);
+        {   int AllDesigns = (N * T * M);
+            System.out.println(AllDesigns);
+        }
+    }
+}
