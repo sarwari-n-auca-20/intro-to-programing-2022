7aaf161047c20d649bbc2f5dfcb31d53f1adb49f
Nargis <sarwari_n@auca.kg>
2022-10-11 22:54:33 +0600

    Accepted oj-midterm's Problem10

---
+++ oj-midterm/src/Problem10.java
@@ -0,0 +1,13 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Problem10 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int v = input.nextInt();
+        if (v >= 1 && v <= Math.pow(10,9)){
+            double t = (2 * v) / (9.8);
+            System.out.printf("%.6f", t);
+        }
+    }
+}
