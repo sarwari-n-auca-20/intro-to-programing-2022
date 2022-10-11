f5c018eecb622be3bc98bcff9aa962d39c19f457
Nargis <sarwari_n@auca.kg>
2022-10-11 23:02:24 +0600

    Accepted oj-midterm's Problem09

--- oj-midterm/src/Problem09.java
+++ oj-midterm/src/Problem09.java
@@ -5,6 +5,8 @@ class Problem09 {
         double r = input.nextDouble();
         double h = input.nextDouble();
         double v = Math.PI * r * r * h / 3;
+        if (h >= 1 & v <= Math.pow(10,2))
+            if (h >= 1 & v <= Math.pow(10,2))
         System.out.printf("%.2f%n", v);
     }
 }
