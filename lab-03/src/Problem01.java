4e752b009edf0eaf47cc6ada884b9af44e48e3a7
Nargis <sarwari_n@auca.kg>
2022-10-13 22:47:17 +0600

    Completed problem01-lab03

---
+++ lab-03/src/Problem01.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Problem01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("some real number? ");
+        double x = input.nextDouble();
+        double r = x;
+        if  (r < 0) {
+            r = -r;
+        }
+        System.out.printf("|%.4f| = %.4f%n", x, r);
+    }
+}
