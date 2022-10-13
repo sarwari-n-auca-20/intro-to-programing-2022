956d688f912e5c2e167f5909711045ed06e81426
Nargis <sarwari_n@auca.kg>
2022-10-14 00:20:02 +0600

    Completed problem06-lab02

--- lab-02/src/Problem06.java
+++ lab-02/src/Problem06.java
@@ -5,6 +5,6 @@ public class Problem06 {
         System.out.print("Enter a degree in Celsius : ");
         double celsius = input.nextDouble();
         double fahrenheit = (9.0/5) * celsius + 32;
-        System.out.println("celsius " + celsius + " is " +  fahrenheit + " in fahrenheit");
+        System.out.println(celsius + " Celsius" + " is " +  fahrenheit + " Fahrenheit");
     }
 }
