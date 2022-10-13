c3a7c03e1be9da3a890d70dcaa2bc9b428cbcc05
Nargis <sarwari_n@auca.kg>
2022-10-13 23:27:20 +0600

    Got the answer for the Problem04 of the oj-midterm

--- oj-midterm/src/Problem04.java
+++ oj-midterm/src/Problem04.java
@@ -1,11 +1,10 @@
 import java.util.Scanner;
 public class Problem04 {
     public static void main(String[] args) {
-        Scanner input = new Scanner (System.in);
-        int a = input.nextInt();
-        int b = input.nextInt();
-        int sum = (a + b);
-        System.out.println(sum);
-
+        Scanner input = new Scanner(System.in);
+        double base = input.nextInt();
+        double height = input.nextInt();
+        double area = (base * height) / 2;
+        System.out.println(area);
     }
 }
