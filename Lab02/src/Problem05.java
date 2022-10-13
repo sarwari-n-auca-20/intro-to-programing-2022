90d52d9305de2b4192c5213dacd8fabf999970a3
Nargis <sarwari_n@auca.kg>
2022-10-13 22:33:35 +0600

    Completed problem05-lab02

---
+++ Lab02/src/Problem05.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Problem05 {
+    public static void main(String[] args) {
+        {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Temperature in degrees Fahrenheit? ");
+            double temperatureFahrenheit = input.nextDouble();
+            double temperatureCelsius =  5.0 / 9 * (temperatureFahrenheit - 32);
+            System.out.printf("The temperature in degrees Celsius is %.2f", temperatureCelsius);
+        }
+    }
+}
