626a41755424782cc8763c889f16904fbf3b5ac9
Nargis <sarwari_n@auca.kg>
2022-10-13 21:59:49 +0600

    Completed problem02-lab02

---
+++ Lab02/src/Problem02.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Problem02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("1st value? ");
+        int a = input.nextInt();
+        System.out.print("2nd value? ");
+        int b = input.nextInt();
+        System.out.printf("Before swapping: a = %d, b = %d;%n", a, b);
+        int c = a;
+        a = b;
+        b = c;
+        System.out.printf("After swapping: a = %d, b = %d;%n", a, b);
+
+    }
+}
