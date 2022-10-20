fff72d968a767fa41bacbc2d71c1b638bb9a8efe
Nargis <sarwari_n@auca.kg>
2022-10-20 20:54:40 +0600

    Got the answer for the Problem21 of the oj-midterm

---
+++ oj-midterm/src/Problem21.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Problem21 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        String output;
+        if (a > b) {
+            output = "1";
+        } else {
+            output = "0";
+        }
+        System.out.println(output);
+    }
+}
