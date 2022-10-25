87ac722b02338964d3ff36c4d758647d5b3c7be6
Nargis <sarwari_n@auca.kg>
2022-10-25 21:29:15 +0600

    Completed problem04-lab03

---
+++ lab-03/src/Problem04.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Problem04 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("year? ");
+        int year = input.nextInt();
+
+        if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0 ) {
+            System.out.println("This year is a leap year");
+        } else {
+            System.out.println("This year is not a leap year");
+        }
+    }
+}
