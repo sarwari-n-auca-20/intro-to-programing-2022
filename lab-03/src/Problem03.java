1ef0881e2aae6201ffc6da8e267889c716966d91
Nargis <sarwari_n@auca.kg>
2022-10-25 21:25:26 +0600

    Completed problem03-lab03

---
+++ lab-03/src/Problem03.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Problem03 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("1st number? ");
+        int a = input.nextInt();
+        System.out.println("2nd number? ");
+        int b = input.nextInt();
+        System.out.println("3rd number? ");
+        int c = input.nextInt();
+
+        if (a >= b) {
+            if (a >= c) {
+                System.out.printf("The value %d is the greatest one%n", a);
+            } else {
+                System.out.printf("The value %d is the greatest one%n", c);
+            }
+        } else {
+            if (b >= c) {
+                System.out.printf("The value %d is the greatest one%n", b);
+            } else {
+                System.out.printf("The value %d is the greatest one%n", c);
+            }
+        }
+
+
+        int maxValue = a;
+        if (maxValue < b) {
+            maxValue = b ;
+        }
+        if (maxValue < c) {
+            maxValue = c;
+        }
+        System.out.printf("The value %d is the greatest one%n", Math.max(a, Math.max(b, c)));
+    }
+}
