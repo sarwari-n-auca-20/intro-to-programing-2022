854a1d4cf205a596e6ab15834e7c1ae5568a6f32
Nargis <sarwari_n@auca.kg>
2022-10-25 19:54:22 +0600

    Got the answer for the Problem25 of the oj-midterm

---
+++ oj-midterm/src/Problem25.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Problem25 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String hissingMic = input.next();
+        if (hissingMic.contains("ss")) {
+            System.out.println("hiss");
+        }else {
+            System.out.println("not hiss");
+        }
+
+    }
+}
