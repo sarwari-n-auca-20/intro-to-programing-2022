c23ddef2c23eaead25e7e697e69790f14fb2255f
Nargis <sarwari_n@auca.kg>
2022-10-25 19:48:43 +0600

    Got the answer for the Problem23 of the oj-midterm

---
+++ oj-midterm/src/Problem23.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Problem23 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int m = input.nextInt(), h = input.nextInt();
+        int total = h * 60 * m - 45;
+        if (total < 0) {
+            total = 1440 + total;
+        }
+        int newh = total / 60;
+        int newm = total % 60;
+        System.out.println(newh + " " + newm);
+    }
+}
