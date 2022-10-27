e4943c35a9923d3459b7ba73b56adb1fd5695b88
Nargis <sarwari_n@auca.kg>
2022-10-27 20:19:32 +0600

    Got the answer for the Problem30 of the oj-midterm

---
+++ oj-midterm/src/Problem30.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Problem30 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int angles1 = input.nextInt();
+        int angles2 = input.nextInt();
+        int angles3 = input.nextInt();
+        if ( angles3 == 60 && angles1 == 60 && angles2 == 60) {
+            System.out.println("Equilateral");
+        } else if (angles3 + angles2 + angles1 == 180 && angles1 == angles2 || angles1 == angles3 ||
+                angles2 == angles3) {
+            System.out.println("Isosceles");
+        } else if (angles3 + angles2 + angles1 == 180) {
+            System.out.println("Scalene");
+        } else if (angles3 + angles2 + angles1 > 180) {
+            System.out.println("Error");
+        }
+    }
+}
