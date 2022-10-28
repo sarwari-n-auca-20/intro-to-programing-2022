1fad17439f4410352ebaa29fcb5dfdfdcb42feb7
Nargis <sarwari_n@auca.kg>
2022-10-28 15:28:13 +0600

    Got the answer for the Problem30 of the oj-midterm

--- oj-midterm/src/Problem30.java
+++ oj-midterm/src/Problem30.java
@@ -6,14 +6,16 @@ public class Problem30 {
         int angles1 = input.nextInt();
         int angles2 = input.nextInt();
         int angles3 = input.nextInt();
+
         if ( angles3 == 60 && angles1 == 60 && angles2 == 60) {
             System.out.println("Equilateral");
-        } else if (angles3 + angles2 + angles1 == 180 && angles1 == angles2 || angles1 == angles3 ||
-                angles2 == angles3) {
+        } else if ((angles3 + angles2 + angles1 == 180) && (angles1 == angles2 || angles1 == angles3 ||
+                angles2 == angles3)){
             System.out.println("Isosceles");
-        } else if (angles3 + angles2 + angles1 == 180) {
+        } else if ((angles3 + angles2 + angles1 == 180) &&
+                (angles1 != angles2 && angles1 != angles3 && angles2 != angles3)){
             System.out.println("Scalene");
-        } else if (angles3 + angles2 + angles1 > 180) {
+        } else if (angles3 + angles2 + angles1 != 180) {
             System.out.println("Error");
         }
     }
