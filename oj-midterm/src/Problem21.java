fd2d7b5fa17fca7b64781ae3ed3a24c267506c79
Nargis <sarwari_n@auca.kg>
2022-11-01 21:52:54 +0600

    Got the answer for the Problem21 of the oj-midterm

--- oj-midterm/src/Problem21.java
+++ oj-midterm/src/Problem21.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class Problem21 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
         int a = input.nextInt();
         int b = input.nextInt();
+
         String output;
         if (a > b) {
             output = "1";
