ede8383f5bfdb7f377e4e186fe194e8da27a3268
Nargis <sarwari_n@auca.kg>
2022-11-01 21:53:49 +0600

    Got the answer for the Problem22 of the oj-midterm

--- oj-midterm/src/Problem22.java
+++ oj-midterm/src/Problem22.java
@@ -1,15 +1,13 @@
 import java.util.Scanner;
-
 public class Problem22 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int oddEven = input.nextInt();
 
-        if (oddEven %2 ==0) {
+        if (oddEven %2 == 0) {
             System.out.println("Bob");
         } else {
             System.out.println("Alice");
         }
-
     }
 }
