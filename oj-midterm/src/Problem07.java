d4c9688abaf25b9cd16619bb5b99ba0c36128c0c
Nargis <sarwari_n@auca.kg>
2022-11-01 18:05:24 +0600

    Got the answer for the Problem07 of the oj-midterm

--- oj-midterm/src/Problem07.java
+++ oj-midterm/src/Problem07.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 public class Problem07 {
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int R1 = input.nextInt();
         int S = input.nextInt();
+
         int R2 = 2*S - R1;
+
         if (R1 >=-1000 && R1 <= 1000)
             if (S >=-1000 && S <= 1000)
         System.out.println(R2);
