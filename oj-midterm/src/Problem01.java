c40bdb007ab3976038fba38cc075ecfd8751d8aa
Nargis <sarwari_n@auca.kg>
2022-11-01 17:47:04 +0600

    Got the answer for the Problem01 of the oj-midterm

--- oj-midterm/src/Problem01.java
+++ oj-midterm/src/Problem01.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 public class Problem01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         String Hello = scanner.next();
         System.out.println(Hello + " " + Hello + " " + Hello);
-
     }
 }
 
