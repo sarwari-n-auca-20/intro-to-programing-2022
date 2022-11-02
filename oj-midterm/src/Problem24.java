a048c60c85d314b80d6ce44f06cffd0a5b8b1f55
Nargis <sarwari_n@auca.kg>
2022-11-02 11:51:16 +0600

    Got the answer for the Problem24 of the oj-midterm

--- oj-midterm/src/Problem24.java
+++ oj-midterm/src/Problem24.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
-
 public class Problem24 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int a = input.nextInt();
         int b = input.nextInt();
         if (a >= b) {
-            System.out.print("b" + " ");
-            System.out.println(a);
-        }else {
-            System.out.print("a" + " ");
-            System.out.println(b);
+            System.out.println(b + " " + a);
+        } else {
+            System.out.println(a + " " + b);
+//            System.out.print(b + " ");
+//            System.out.println(a);
+//        }else {
+//            System.out.print(a + " ");
+//            System.out.println(b);
         }
     }
 }
