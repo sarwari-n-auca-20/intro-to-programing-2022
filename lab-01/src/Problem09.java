8e271209c5698a88a97a2ff4e21e1235c3ec0d5e
Nargis <sarwari_n@auca.kg>
2022-11-02 09:28:42 +0600

    Completed problem09-lab01

--- lab-01/src/Problem09.java
+++ lab-01/src/Problem09.java
@@ -1,11 +1,9 @@
 import java.sql.SQLOutput;
-
 public class Problem09 {
     public static void main(String[] args) {
         double PI = 4 * (1.0-1.0/3+1.0/5-1.0/7+1.0/9-1.0/11);
         System.out.println(PI);
         double pi = 4 * (1.0-1.0/3+1.0/5-1.0/7+1.0/9-1.0/11+1.0/13);
         System.out.println(pi);
-
     }
 }
