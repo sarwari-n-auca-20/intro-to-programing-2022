754519a607d5c07efb59b8f2eed185aefed024a9
Nargis <sarwari_n@auca.kg>
2022-11-02 11:06:28 +0600

    Completed problem09-lab02

--- lab-02/src/Problem09.java
+++ lab-02/src/Problem09.java
@@ -1,14 +1,21 @@
 import java.util.Scanner;
-
 public class Problem09 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the number of minutes: ");
         long minutes = input.nextLong();
+
         long years = minutes / (365 * 24 * 60);
-        long yearsToMins = years * 525600;
-        long roMin = minutes - yearsToMins;
-        long days = roMin / (24 * 60);
-        System.out.println(minutes  + " minutes " + " is approximately " + years + " years and " + days + " days ");
+        long yearsToMin = years * 525600;
+        long reMin = minutes - yearsToMin;
+        long days = reMin / (24 * 60);
+
+        System.out.println(minutes + " minutes " + "is approximately " + years + " years and " + days + " days ");
+//        long minutes = input.nextLong();
+//        long years = minutes / (365 * 24 * 60);
+//        long yearsToMin = years * 525600;
+//        long roMin = minutes - yearsToMin;
+//        long days = roMin / (24 * 60);
+//        System.out.println(minutes  + " minutes " + " is approximately " + years + " years and " + days + " days ");
     }
 }
