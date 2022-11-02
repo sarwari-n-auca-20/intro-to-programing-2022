da61875b211367e38f892fd7b7d936c8652c2ba7
Nargis <sarwari_n@auca.kg>
2022-11-02 10:51:22 +0600

    Completed problem07-lab02

--- lab-02/src/Problem07.java
+++ lab-02/src/Problem07.java
@@ -1,16 +1,26 @@
 import java.util.Scanner;
-
 public class Problem07 {
     public static void main(String[] args) {
         Scanner input = new Scanner (System.in);
         System.out.print("Enter the radius and length of a cylinder: ");
+
         double radius = input.nextDouble();
         double length = input.nextDouble();
         double area = radius * radius * 3.14159;
-        double volume =  area * length;
+        double volume = area * length;
+
         System.out.print("The area is ");
         System.out.printf("%.4f%n", area);
         System.out.print("The volume is ");
-        System.out.printf("%.2f%n", volume);
+        System.out.printf("%.1f%n", volume);
+//        System.out.print("Enter the radius and length of a cylinder: ");
+//        double radius = input.nextDouble();
+//        double length = input.nextDouble();
+//        double area = radius * radius * 3.14159;
+//        double volume =  area * length;
+//        System.out.print("The area is ");
+//        System.out.printf("%.4f%n", area);
+//        System.out.print("The volume is ");
+//        System.out.printf("%.2f%n", volume);
     }
 }
