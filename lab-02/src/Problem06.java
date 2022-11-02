46971a33b042e2c34a128d3772c05c77687302ab
Nargis <sarwari_n@auca.kg>
2022-11-02 10:45:22 +0600

    Completed problem06-lab02

--- lab-02/src/Problem06.java
+++ lab-02/src/Problem06.java
@@ -2,9 +2,14 @@ import java.util.Scanner;
 public class Problem06 {
     public static void main(String[] args) {
         Scanner input = new Scanner (System.in);
-        System.out.print("Enter a degree in Celsius : ");
-        double celsius = input.nextDouble();
-        double fahrenheit = (9.0/5) * celsius + 32;
-        System.out.println(celsius + " Celsius" + " is " +  fahrenheit + " Fahrenheit");
+        System.out.print("Enter a degree in celsius: ");
+        double Celsius = input.nextDouble();
+
+        double degreeInFahrenheit = (9.0/5) * Celsius + 32;
+        System.out.println(Celsius + " Celsius " + "is " + degreeInFahrenheit + " Fahrenheit" );
+//        System.out.print("Enter a degree in Celsius : ");
+//        double celsius = input.nextDouble();
+//        double fahrenheit = (9.0/5) * celsius + 32;
+//        System.out.println(celsius + " Celsius" + " is " +  fahrenheit + " Fahrenheit");
     }
 }
