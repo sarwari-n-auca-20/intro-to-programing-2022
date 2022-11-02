e79203c9e0dd38e4979abb419e91f0537da4815f
Nargis <sarwari_n@auca.kg>
2022-11-02 11:00:37 +0600

    Completed problem10-lab02

--- lab-02/src/Problem10.java
+++ lab-02/src/Problem10.java
@@ -11,15 +11,15 @@ public class Problem10 {
         double birth = SEC_IN_YEAR / 7.0;
         double death = SEC_IN_YEAR / 13.0;
         double migration = SEC_IN_YEAR / 45.0;
-        double yearlypopulationgrowth = birth - death + migration;
+        double yearlyPopulationGrowth = birth - death + migration;
        //int popInt1Year = (int) Math.round(POPULATION +yearlypopulationgrowth);
         //int popInt2Year = (int) Math.round(POPULATION + yearlypopulationgrowth * 2);
         //int popInt3Year = (int) Math.round(POPULATION + yearlypopulationgrowth * 3);
         //int popInt4Year = (int) Math.round(POPULATION + yearlypopulationgrowth * 4);
         //int popInt5Year = (int) Math.round(POPULATION + yearlypopulationgrowth * 5);
-        int popIntnYear = (int) Math.round(POPULATION + yearlypopulationgrowth * numberOfYears);
+        int popInnYear = (int) Math.round(POPULATION + yearlyPopulationGrowth * numberOfYears);
 
-        System.out.println("The population in 5 year is " + popIntnYear + "\n");
+        System.out.println("The population in 5 year is " + popInnYear + "\n");
                         //popInt2Year + "\n" +
                         //popInt3Year + "\n" +
                         //popInt4Year + "\n" +
