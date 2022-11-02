e0bf1658fe0b6e89ddaeebcb49dacd02d4a3c866
Nargis <sarwari_n@auca.kg>
2022-11-02 09:57:57 +0600

    Completed problem10-lab01

--- lab-01/src/Problem10.java
+++ lab-01/src/Problem10.java
@@ -1,24 +1,44 @@
 public class Problem10 {
     public static void main(String[] args) {
-        final int POPULATION = 312_032_486;
-        final int  SEC_IN_YEAR = 365 * 24 * 60 * 60;
+      final int POPULATION = 312_032_486;
+      final int SEC_IN_YEAR = 365 * 24 * 60 * 60;
 
-        double birth = SEC_IN_YEAR / 7.0;
-        double death = SEC_IN_YEAR / 13.0;
-        double migration = SEC_IN_YEAR / 45.0;
-        double yearlypopulationgrowth = birth - death + migration;
-        int popInt1Year = (int) Math.round(POPULATION +yearlypopulationgrowth);
-        int popInt2Year = (int) Math.round(POPULATION + yearlypopulationgrowth * 2);
-        int popInt3Year = (int) Math.round(POPULATION + yearlypopulationgrowth * 3);
-        int popInt4Year = (int) Math.round(POPULATION + yearlypopulationgrowth * 4);
-        int popInt5Year = (int) Math.round(POPULATION + yearlypopulationgrowth * 5);
+      double birth = SEC_IN_YEAR / 7.0;
+      double death = SEC_IN_YEAR / 13.0;
+      double migration = SEC_IN_YEAR / 45.0;
+      double yearlyPopulationGrowth = birth + migration -death;
 
-        System.out.println(
-                popInt1Year + "\n" +
-                        popInt2Year + "\n" +
-                        popInt3Year + "\n" +
-                        popInt4Year + "\n" +
-                        popInt5Year
-        );
+      int pop1stYear = (int) Math.round(POPULATION + yearlyPopulationGrowth);
+      int pop2ndYear = (int) Math.round(POPULATION + 2 * yearlyPopulationGrowth);
+      int pop3rdYear = (int) Math.round(POPULATION + 3 * yearlyPopulationGrowth);
+      int pop4thYear = (int) Math.round(POPULATION + 4 * yearlyPopulationGrowth);
+      int pop5thYear = (int) Math.round(POPULATION + 5 * yearlyPopulationGrowth);
+
+        System.out.println(pop1stYear + "\n" +
+                pop2ndYear + "\n" +
+                pop3rdYear + "\n" +
+                pop4thYear + "\n" +
+                pop5thYear)
+        ;
+//        final int POPULATION = 312_032_486;
+//        final int  SEC_IN_YEAR = 365 * 24 * 60 * 60;
+//
+//        double birth = SEC_IN_YEAR / 7.0;
+//        double death = SEC_IN_YEAR / 13.0;
+//        double migration = SEC_IN_YEAR / 45.0;
+//        double yearlyPopulationGrowth = birth - death + migration;
+//        int popInt1Year = (int) Math.round(POPULATION +yearlyPopulationGrowth);
+//        int popInt2Year = (int) Math.round(POPULATION + yearlyPopulationGrowth * 2);
+//        int popInt3Year = (int) Math.round(POPULATION + yearlyPopulationGrowth * 3);
+//        int popInt4Year = (int) Math.round(POPULATION + yearlyPopulationGrowth * 4);
+//        int popInt5Year = (int) Math.round(POPULATION + yearlyPopulationGrowth * 5);
+//
+//        System.out.println(
+//                popInt1Year + "\n" +
+//                        popInt2Year + "\n" +
+//                        popInt3Year + "\n" +
+//                        popInt4Year + "\n" +
+//                        popInt5Year
+//        );
         }
     }
