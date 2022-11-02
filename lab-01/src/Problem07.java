f6496a043602ec20a31665470e2a4403caf0dc9d
Nargis <sarwari_n@auca.kg>
2022-11-02 09:26:58 +0600

    Completed problem07-lab01

--- lab-01/src/Problem07.java
+++ lab-01/src/Problem07.java
@@ -1,13 +1,25 @@
 public class Problem07 {
     public static void main(String[] args) {
-        System.out.println("a    a^2    a^3");
-        System.out.print(1);System.out.print("    ");System.out.print((1*1));System.out.print("      ");
-        System.out.println((1*1*1));
-        System.out.print(2);System.out.print("    ");System.out.print((2*2));System.out.print("      ");
-        System.out.println(2*2*2);
-        System.out.print(3); System.out.print("    "); System.out.print((3*3));System.out.print("      ");
-        System.out.println((3*3*3));
-        System.out.print(4); System.out.print("    "); System.out.print((4*4)); System.out.print("     ");
-        System.out.print((4*4*4));
+        System.out.println("a   a^2   a^3");
+        System.out.print(1); System.out.print("   "); System.out.print((1*1));
+        System.out.print("   ");System.out.println((1*1*1));
+
+        System.out.print(2); System.out.print("   "); System.out.print((2*2));
+        System.out.print("   ");System.out.println((2*2*2));
+
+        System.out.print(3); System.out.print("   "); System.out.print((3*3));
+        System.out.print("   ");System.out.println((3*3*3));
+
+        System.out.print(4); System.out.print("   "); System.out.print((4*4));
+        System.out.print("   ");System.out.println((4*4*4));
+//        System.out.println("a    a^2    a^3");
+//        System.out.print(1);System.out.print("    ");System.out.print((1*1));System.out.print("      ");
+//        System.out.println((1*1*1));
+//        System.out.print(2);System.out.print("    ");System.out.print((2*2));System.out.print("      ");
+//        System.out.println(2*2*2);
+//        System.out.print(3); System.out.print("    "); System.out.print((3*3));System.out.print("      ");
+//        System.out.println((3*3*3));
+//        System.out.print(4); System.out.print("    "); System.out.print((4*4)); System.out.print("     ");
+//        System.out.print((4*4*4));
     }
 }
