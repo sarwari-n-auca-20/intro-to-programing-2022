064fba1a8e9e820798421a76b3733a913c8d5e31
Nargis <sarwari_n@auca.kg>
2022-11-02 08:03:31 +0600

    Completed problem02-lab01

--- lab-01/src/Problem02.java
+++ lab-01/src/Problem02.java
@@ -1,7 +1,10 @@
 public class Problem02 {
     public static void main(String[] args) {
-        System.out.println("********************");
-        System.out.println("*I like programing!*");
-        System.out.println("********************");
+//        System.out.println("********************");
+//        System.out.println("*I like programing!*");
+//        System.out.println("********************");
+          System.out.println("********************\n"+
+                            "*I like programing!*\n" +
+                             "********************");
     }
 }
