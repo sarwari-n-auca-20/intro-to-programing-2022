432c51934476323f060317331fd27100a3979369
Nargis <sarwari_n@auca.kg>
2022-11-02 10:30:37 +0600

    Completed problem03-lab02

--- lab-02/src/Problem03.java
+++ lab-02/src/Problem03.java
@@ -1,19 +1,36 @@
+import java.security.spec.RSAOtherPrimeInfo;
 import java.util.Scanner;
-
 public class Problem03 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("A four digit integer? ");
+
         int n = input.nextInt();
         int sumOfDigits = 0;
-        sumOfDigits += n % 10;
+
+        sumOfDigits += n%10;
         n /= 10;
-        sumOfDigits += n % 10;
+
+        sumOfDigits += n%10;
         n /= 10;
-        sumOfDigits += n % 10;
+
+        sumOfDigits += n%10;
         n /= 10;
-        sumOfDigits += n % 10;
+
+        sumOfDigits += n%10;
         n /= 10;
+
         System.out.println("The sum of all digits is " + sumOfDigits);
+//        int n = input.nextInt();
+//        int sumOfDigits = 0;
+//        sumOfDigits += n % 10;
+//        n /= 10;
+//        sumOfDigits += n % 10;
+//        n /= 10;
+//        sumOfDigits += n % 10;
+//        n /= 10;
+//        sumOfDigits += n % 10;
+//        n /= 10;
+//        System.out.println("The sum of all digits is " + sumOfDigits);
     }
 }
