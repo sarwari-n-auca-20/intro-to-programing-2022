7004255c7d64e152dbb36fcce8f88c19938e8a00
Nargis <sarwari_n@auca.kg>
2022-11-02 11:41:48 +0600

    Got the answer for the Problem16 of the oj-midterm

--- oj-midterm/src/Problem16.java
+++ oj-midterm/src/Problem16.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
-
 public class Problem16 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int num1 = input.nextInt();
         int num2 = input.nextInt();
         String isMultiple;
-        isMultiple = "no";
         if (num1 % num2 == 0) {
             isMultiple = "yes";
+        } else {
+            isMultiple = "no";
         }
         System.out.println(isMultiple);
 
