c8a93e5c668d9e463b7aca973cef91a1a3dad759
Nargis <sarwari_n@auca.kg>
2022-11-02 09:13:25 +0600

    Completed problem06-lab01

--- lab-01/src/Problem06.java
+++ lab-01/src/Problem06.java
@@ -1,10 +1,13 @@
 import java.sql.SQLOutput;
-
 public class Problem06 {
     public static void main(String[] args) {
-        System.out.println("    J     A    V      V    A     ");
-        System.out.println("    J    A A    V    V    A A    ");
-        System.out.println("J   J   AAAAA    V  V    AAAAA   ");
-        System.out.println(" J J   A     A    V     A     A  ");
+        System.out.println("    J     A    V      V    A     \n"+
+                "    J    A A    V    V    A A    \n" +
+                "J   J   AAAAA    V  V    AAAAA   \n" +
+                " J J   A     A    V     A     A  ");
+//        System.out.println("    J     A    V      V    A     ");
+//        System.out.println("    J    A A    V    V    A A    ");
+//        System.out.println("J   J   AAAAA    V  V    AAAAA   ");
+//        System.out.println(" J J   A     A    V     A     A  ");
     }
 }
