f7265127f40d473a52b31e0a842a3ea6ad4a1497
Nargis <sarwari_n@auca.kg>
2022-11-22 16:22:58 +0600

    Completed problem07-lab05

---
+++ lab-05/src/Problem07.java
@@ -0,0 +1,25 @@
+public class Problem07 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int number = 0;
+        while (number < 20) {
+            number++;
+            sum += number;
+            if (sum >= 100)
+                break;
+        }
+        System.out.println("The number is " + number);
+        System.out.println("The sum is " + sum);
+    }
+
+//    int sum = 0;
+//    int number = 0;
+//    while (number < 20) {
+//        number++;
+//        if (number == 10 || number == 11)
+//            continue;
+//        sum += number;
+//    }
+//    System.out.println("The sum is "+ sum);
+//}
+}
